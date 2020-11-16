482c754aaa46eeeec7ae89c4082ad747287b7bfb
Miu Mau <zups@mybeautifulcat.localdomain>
2020-11-16 23:37:36 +0200

    MURR

--- lib/koans/18_genservers.ex
+++ lib/koans/18_genservers.ex
@@ -95,12 +95,12 @@ defmodule GenServers do
 
   koan "Servers that are created and initialized successfully returns a tuple that holds the PID of the server" do
     {:ok, pid} = GenServer.start_link(Laptop, "3kr3t!")
-    assert is_pid(pid) == ___
+    assert is_pid(pid) == true
   end
 
   koan "The handle_call callback is synchronous so it will block until a reply is received" do
     {:ok, pid} = GenServer.start_link(Laptop, "3kr3t!")
-    assert GenServer.call(pid, :get_password) == ___
+    assert GenServer.call(pid, :get_password) == "3kr3t!"
   end
 
   koan "A server can support multiple actions by implementing multiple handle_call functions" do
@@ -117,8 +117,7 @@ defmodule GenServers do
     assert graphics == ___
   end
 
-  koan "The handle_cast callback handles asynchronous messages" do
-    {:ok, pid} = GenServer.start_link(Laptop, "3kr3t!")
+  koan "The handle_cast callback handles asynchronous messages" do {:ok, pid} = GenServer.start_link(Laptop, "3kr3t!")
     GenServer.cast(pid, {:change_password, "3kr3t!", "73x7!n9"})
     assert GenServer.call(pid, :get_password) == ___
   end
